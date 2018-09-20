dfb367481d57bf88337bc475433719a7094f0483
Michael Crosby <crosbymichael@gmail.com>
2018-09-20 09:08:15 -0400

    Initial copy from boss

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "vab"
+	app.Version = "1"
+	app.Usage = "run containers like a ross"
+	app.Description = `
+        _..-.._
+     .'  _   _  ` + "`" + `.
+    /_) (_) (_) (_\
+   /               \
+   |'''''''''''''''|
+  /                 \
+ |                   |
+ |-------------------|
+ |                   |
+ |                   |
+ |'''''''''''''''''''|
+ |             .--.  |
+ |            //  \\=|
+ |            ||- || |
+ |            \\__//=|
+ |             '--'  |
+ |...................|
+ |___________________|
+ |___________________|
+ |___________________|
+ |___________________|
+   /_______________\
+
+container assembly builder`
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "enable debug output in the logs",
+		},
+		cli.StringFlag{
+			Name:   "buildkit,b",
+			Usage:  "buildkit address",
+			Value:  "127.0.0.1:9200",
+			EnvVar: "BUILDKIT",
+		},
+	}
+	app.Before = func(clix *cli.Context) error {
+		if clix.GlobalBool("debug") {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		return nil
+	}
+	app.Commands = []cli.Command{
+		buildCommand,
+	}
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
